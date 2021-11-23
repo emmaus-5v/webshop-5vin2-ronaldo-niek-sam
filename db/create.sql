5a90b821b95bb7a4fc365c541dcd8ddcf509421a
SamHerberigs <112369@emmauscollege.nl>
2021-11-23 21:15:23 +0000

    database uitbreiding

--- db/create.sql
+++ db/create.sql
@@ -9,10 +9,35 @@ CREATE TABLE products (
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2),
-  age_restriction NUMERIC(10, 2),
-  genre VARCHAR(255)
+  recommended_minimum_age_id NUMERIC(10, 2),
+  rating_id NUMERIC(10, 2)
 );
 
+DROP TABLE IF EXISTS genres; 
+CREATE TABLE genres (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255),
+  description TEXT
+);
+
+DROP TABLE IF EXISTS product_genres;
+CREATE TABLE product_genres (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  product_id NUMERIC(10, 2),
+  genre_id NUMERIC(10, 2)
+);
+
+DROP TABLE IF EXISTS recommended_minimum_ages; 
+CREATE TABLE recommended_minimum_ages (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  recommended_minimum_age VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS ratings; 
+CREATE TABLE ratings (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  rating VARCHAR(255)
+);
 
 
 
@@ -25,11 +50,7 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, age_restriction, genre, code, price) values ('French Film', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
-
-Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
-
-In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.', 16, 'actie & horror', '816905633-0', 10.5);
+insert into products (name, description, recommended_minimum_age_id,rating_id, code, price) values ('French Film', 'franse film bruh', 3, 5, '816905633-0', 10.5);
 insert into products (name, description, age_restriction, genre, code, price) values ('Post Mortem', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.
 
 Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.
@@ -50,11 +71,42 @@ insert into products (name, description, code, price) values ('Velvet Goldmine',
 
 Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '492662523-7', 14);
 
-DROP TABLE IF EXISTS genres; 
-CREATE TABLE genres (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(255),
-  description TEXT
-);
 
-insert into genres (name, description) values ('action', 'boom boom pow pow');
+/* #1 */ insert into genres (name, description) values ('action', 'boom boom pow pow'); 
+/* #2 */ insert into genres (name, description) values ('comedy', 'ksu ksu ksu');
+/* #3 */ insert into genres (name, description) values ('documentary', 'David Attenborough');
+/* #4 */ insert into genres (name, description) values ('animated movie', 'disney etc.');
+/* #5 */ insert into genres (name, description) values ('romance', 'awww');
+/* #6 */ insert into genres (name, description) values ('sci-fi', 'pog genre');
+/* #7 */ insert into genres (name, description) values ('horror', 'aaaahhhhh');
+/* #8 */ insert into genres (name, description) values ('drama', " :'( ");
+/* #9 */ insert into genres (name, description) values ('adventure', 'monster, treasure, evil organisation that wants treasure, more treasure');
+/* #10 */ insert into genres (name, description) values ('fantasy', 'skidadle dkidoodle, your...');
+/* #11 */ insert into genres (name, description) values ('superhero', '...man, ...woman, ...boy and ...girl. Very original names');
+/* #12 */ insert into genres (name, description) values ('mystery', '?');
+/* #13 */ insert into genres (name, description) values ('history', 'yesterday, the day before yesterday, the day before the day before yesterday, ...');
+
+insert into ratings (rating) values ('1/10 penguins');
+insert into ratings (rating) values ('2/10 penguins');
+insert into ratings (rating) values ('3/10 penguins');
+insert into ratings (rating) values ('4/10 penguins');
+insert into ratings (rating) values ('5/10 penguins');
+insert into ratings (rating) values ('6/10 penguins');
+insert into ratings (rating) values ('7/10 penguins');
+insert into ratings (rating) values ('8/10 penguins');
+insert into ratings (rating) values ('9/10 penguins');
+insert into ratings (rating) values ('10/10 penguins');
+insert into ratings (rating) values ('-1/10 penguins');
+insert into ratings (rating) values ('-2/10 penguins');
+insert into ratings (rating) values ('-3/10 penguins');
+
+/* #1 */ insert into recommended_minimum_ages (recommended_minimum_age) values ('ALL');
+/* #2 */ insert into recommended_minimum_ages (recommended_minimum_age) values ('9');
+/* #3 */ insert into recommended_minimum_ages (recommended_minimum_age) values ('13');
+/* #4 */ insert into recommended_minimum_ages (recommended_minimum_age) values ('16');
+/* #5 */ insert into recommended_minimum_ages (recommended_minimum_age) values ('18');
+/* #6 */ insert into recommended_minimum_ages (recommended_minimum_age) values ('21');
+
+insert into product_genres (product_id, genre_id) values (1, 3);
+insert into product_genres (product_id, genre_id) values (1, 11);
+insert into product_genres (product_id, genre_id) values (1, 13);

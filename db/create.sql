cf11eb854efde273c075822dfa2a3591033e9c99
SamHerberigs <112369@emmauscollege.nl>
2021-12-19 21:53:59 +0000

    OwO

--- db/create.sql
+++ db/create.sql
@@ -50,12 +50,18 @@ CREATE TABLE ratings (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, recommended_minimum_age_id, rating_id, code, price) values ('film 1', '...', 1, 9, '816905633-0', 10.5);
-insert into products (name, description, recommended_minimum_age_id, rating_id, code, price) values ('film 2', '...', 7, 6, '077030122-3', 11);
-insert into products (name, description, recommended_minimum_age_id, rating_id, code, price) values ('film 3', '...', 4, 8, '445924201-X', 13.5);
-insert into products (name, description, recommended_minimum_age_id, rating_id, code, price) values ('film 4', '...', 6, 12, '693155505-7', 13.5);
-insert into products (name, description, recommended_minimum_age_id, rating_id, code, price) values ('film 5', '...', 2, 2, '686928463-6', 14);
-insert into products (name, description, recommended_minimum_age_id, rating_id, code, price) values ('film 6', '...', 5, 10, '492662523-7', 14);
+insert into products (name, description, recommended_minimum_age_id, rating_id, code, price) values ('Lord of the Drinks', '...', 1, 9, '816905633-0', 10.5);
+insert into products (name, description, recommended_minimum_age_id, rating_id, code, price) values ('Tinytanic', '...', 7, 6, '077030122-3', 11);
+insert into products (name, description, recommended_minimum_age_id, rating_id, code, price) values ('Soy Story 3', '...', 4, 8, '445924201-X', 13.5);
+insert into products (name, description, recommended_minimum_age_id, rating_id, code, price) values ('Jurassic Shark', '...', 6, 12, '693155505-7', 13.5);
+insert into products (name, description, recommended_minimum_age_id, rating_id, code, price) values ('Black Pfanzer', '...', 2, 2, '686928463-6', 14);
+insert into products (name, description, recommended_minimum_age_id, rating_id, code, price) values ('The Goatfather', '...', 6, 10, '492662523-7', 14);
+insert into products (name, description, recommended_minimum_age_id, rating_id, code, price) values ('Bar Wars', '...', 7, 10, '194695103-C', 13.5);
+insert into products (name, description, recommended_minimum_age_id, rating_id, code, price) values ('Raiders of the Lost Ark', '...', 5, 7, '193729465-Y', 12);
+insert into products (name, description, recommended_minimum_age_id, rating_id, code, price) values ('Mad Max: Fury Toad', '...', 7, 9, '104639295-3', 15);
+insert into products (name, description, recommended_minimum_age_id, rating_id, code, price) values ('The Lizard of Oz', '...', 3, 5, '103849375-H', 9);
+insert into products (name, description, recommended_minimum_age_id, rating_id, code, price) values ('The Matrucks', '...', 4, 6, '284638568-8', 11);
+
 
 
 /* #1 */ insert into genres (genre, description) values ('action', 'boom boom pow pow'); 
@@ -63,7 +69,7 @@ insert into products (name, description, recommended_minimum_age_id, rating_id, 
 /* #3 */ insert into genres (genre, description) values ('documentary', 'David Attenborough');
 /* #4 */ insert into genres (genre, description) values ('animated movie', 'disney etc.');
 /* #5 */ insert into genres (genre, description) values ('romance', 'smooch');
-/* #6 */ insert into genres (genre, description) values ('sci-fi', 'poep genre');
+/* #6 */ insert into genres (genre, description) values ('sci-fi', 'pog genre');
 /* #7 */ insert into genres (genre, description) values ('horror', 'aaaagggggghhhhh');
 /* #8 */ insert into genres (genre, description) values ('drama', " :'( ");
 /* #9 */ insert into genres (genre, description) values ('adventure', 'monster, treasure, evil organisation that wants treasure, more treasure');

f2595bd00e2539be8bd35691ecd49398f5673b44
Ronaldo561 <112427@emmauscollege.nl>
2021-12-23 14:49:55 +0000

    css update

--- api/index.js
+++ api/index.js
@@ -109,9 +109,10 @@ function getProductGenres(request, response) {
   const product_id = parseInt(request.query.product_id)
   let data = []
   if (product_id > 0) {
-    const sqlOpdracht = db.prepare(`SELECT * FROM product_genres
+    const sqlOpdracht = db.prepare(`SELECT genre FROM product_genres
                                     JOIN genres ON product_genres.genre_id = genres.id
-                                    WHERE product_id = ?
+                                    JOIN products ON product_genres.product_id = products.id
+                                    WHERE products.id = ?
                                     ORDER BY id ASC`
    )
     data = sqlOpdracht.all(product_id)
